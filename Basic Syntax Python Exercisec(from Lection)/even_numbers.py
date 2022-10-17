7e7941cad116deee4a8063dd8a2de98a2f000a8c
Ivan Sharkov <114100694+vatci98@users.noreply.github.com>
2022-10-17 20:01:40 +0300

    Delete Basic Syntax Python Exercisec(from Lection) directory

--- Basic Syntax Python Exercisec(from Lection)/even_numbers.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-for _ in range(n):
-    number = int(input())
-    if not number % 2 == 0:
-        print(f"{number} is odd!")
-        break
-else:
-    print('All numbers are even.')
--- Basic Syntax Python Exercisec(from Lection)/largest_of_three_numbers.py
+++
@@ -1,2 +0,0 @@
-first_num, second_num, third_num = int(input()), int(input()), int(input())
-print(max(first_num, second_num, third_num))
